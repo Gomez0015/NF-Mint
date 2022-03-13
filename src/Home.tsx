b4995853b697e10e7730f5699f69b11c4e4ceec8
Oscar Gomez <56361339+Gomez0015@users.noreply.github.com>
2022-03-13 18:30:54 +0100

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -512,7 +512,7 @@ const Home = (props: HomeProps) => {
                 <MintContainer>
                     <DesContainer>
                         <NFT elevation={3}>
-                            <h2>My NFT</h2>
+                            <h2>Notify France DAO Access Pass</h2>
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
